2ccdc04872474d70cd9258f6e91ff6a0522ce7c8
Ezra Jeonadab G. Del Rosario <ezrajeonadabg@gmail.com>
2024-06-26 05:05:10 +0800

    added main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 /**
+ * Main class that will create and instance of the reservation system and call the necessary methods.
  * 
  * @author Ethan Axl S. Burayag
  * @author Ezra Jeonadab G. Del Rosario
  */
-
 public class Main {
 
     public static void main(String[] args) {
