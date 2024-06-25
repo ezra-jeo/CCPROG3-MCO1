cd18fa854e2e7caf94c45beef8933a0249f32d04
Ezra Jeonadab G. Del Rosario <ezrajeonadabg@gmail.com>
2024-06-26 04:56:58 +0800

    added main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Ethan Axl S. Burayag
+ * @author Ezra Jeonadab G. Del Rosario
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        ReservationSystem rs = new ReservationSystem();
+        rs.runSystem();
+    }
+}
